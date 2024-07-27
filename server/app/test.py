f7c72911e30557e644384a4c5a820783c5a1c29a
赵晗瑜 <2038290448@qq.com>
2024-07-27 21:44:58 +0800

    update

--- server/app/test.py
+++ server/app/test.py
@@ -2,12 +2,14 @@ import torch
 import numpy as np
 from scipy import sparse
 
-model = torch.load(f='../app/model-100/result.pt')
-question2idx = np.load('../app/data/question2idx.npy',
+question2idx = np.load('./data/question2idx.npy',
                        allow_pickle=True).item()
-idx2question = np.load('../app/data/idx2question.npy',
+idx2question = np.load('./data/idx2question.npy',
                        allow_pickle=True).item()
-model = torch.load(f='../app/model-100/result.pt')
-qq_table = sparse.load_npz('../app/data/qq_table.npz').toarray()
-qs_table = sparse.load_npz('../app/data/qs_table.npz').toarray()
-ss_table = sparse.load_npz('../app/data/ss_table.npz').toarray()
+model = torch.load(f='./model-100/result.pt')
+qq_table = sparse.load_npz('./data/qq_table.npz').toarray()
+qs_table = sparse.load_npz('./data/qs_table.npz').toarray()
+ss_table = sparse.load_npz('./data/ss_table.npz').toarray()
+
+
+print(model)
